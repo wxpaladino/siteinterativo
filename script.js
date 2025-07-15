72ef53670a9f4d0e8c705b9988c4a85a1847cbdd
paladino css <143850092+wxpaladino@users.noreply.github.com>
2025-07-15 17:03:27 -0300

    .

---
+++ script.js
@@ -0,0 +1,24 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: Insira texto
+        alternativas: [
+            "alternativa 1"
+            "alternativa 2"
+
+        ]
+    },
+    {
+         enunciado: Insira texto
+        alternativas: [
+            "alternativa 1"
+            "alternativa 2"
+
+        ]
+    }
+]
