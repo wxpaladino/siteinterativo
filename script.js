2305f6bc0b47914897602c3330c9ce8fe3aee961
paladino css <143850092+wxpaladino@users.noreply.github.com>
2025-07-29 11:42:18 -0300

    .

--- script.js
+++ script.js
@@ -6,19 +6,70 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: Insira texto
-        alternativas: [
-            "alternativa 1"
-            "alternativa 2"
-
+        enunciado: "Você se depara com uma oportunidade incrível de conhecer uma nova cultura. Qual destino você deseja visitar?"
+            alternativas: [
+            {
+            texto: "Estados Unidos",
+            afirmacao: "afirmação",
+            },
+             {
+            texto: "Paris",
+            afirmacao: "afirmação",
+            },
         ]
     },
-    {
-         enunciado: Insira texto
-        alternativas: [
-            "alternativa 1"
-            "alternativa 2"
+    
 
+    {
+        enunciado: "Nesse momento você deseja conhecer mais sobre a cultura, então decide visitar algum monumento histórico. Qual você escolhe?"
+            alternativas: [
+            {
+            texto: "Estátua da Liberdade",
+            afirmacao: "afirmação",
+            },
+             {
+            texto: "Museu do Louvre",
+            afirmacao: "afirmação",
+            },
         ]
-    }
-]
+    },
+
+
+     {
+        enunciado: "Você irá se comunicar com um estrangeiro. Qual idioma você utiliza?"
+            alternativas: [
+            {
+            texto: "Inglês",
+            afirmacao: "afirmação",
+            },
+             {
+            texto: "Francês",
+            afirmacao: "afirmação",
+            },
+        ]
+    },
+
+
+     {
+        enunciado: "Você quer visitar um restaurante famoso com pratos típicos da cultura local. Qual você escolhe?"
+            alternativas: [
+            {
+            texto: "Cheesecake",
+            afirmacao: "afirmação",
+            },
+             {
+            texto: "Ratatouille",
+            afirmacao: "afirmação",
+            },
+        ]
+    },
+
+let atual = 0;
+let perguntaAtual; 
+
+function mostraPergunta () {
+    perguntaAtual = pergunta [atual];
+    caixaPerguntas.textContent = perguntas.enunciado;
+}
+
+mostraPergunta ();
